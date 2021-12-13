445557eb9bace9a91c974dfddd3cc80a6f528fdb
earoglif <earoglif@gmail.com>
2021-12-13 23:00:30 +0300

    Бинарный поиск. Функция завершена. Начало работы перевода в класс.

--- binarySearch.js
+++ binarySearch.js
@@ -1,15 +1,32 @@
+/** Бинарный поиск */
+
 const searchTest = [1, 3, 4, 5, 8, 13, 17, 20, 43, 45, 46, 48];  // 12
 
-const binarySearch = ( arr, searchNumber, start = 0, end ) => {
-    const searchLastIndex = end || arr.length - 1
-    const mid = arr[ Math.round( searchLastIndex / 2 ) ];
+class BinarySearch {
+    constructor( arr, searchNumber ) {
+        this.arr = arr;
+        this.searchNumber = searchNumber;
+        this.startIndex = 0;
+        this.endIndex = arr.length - 1;
+        this.iteration = 0;
+    }
+
+    search( searchNumber ) {
+
+    }
+}
+
+const binarySearch = ( arr, searchNumber, start = 0, end, iteration = 1 ) => {
+    const endIndex = end || arr.length - 1;
+    const midIndex = Math.round( ( start + endIndex ) / 2 );
+    const mid = arr[ midIndex ];
 
     if ( mid === searchNumber ) {
-        return mid;
+        console.log( 'Result:', mid, midIndex, iteration );
     } else if( mid < searchNumber ) {
-
+        binarySearch( arr, searchNumber, midIndex, endIndex, ++iteration );
     } else {
-
+        binarySearch( arr, searchNumber, start, midIndex, ++iteration );
     }
 }
 
